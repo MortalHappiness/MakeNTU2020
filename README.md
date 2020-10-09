501f7545e4c1144c466c997cc086b20f2781c71f
MortalHappiness <b07901069@ntu.edu.tw>
2020-10-09 18:40:36 +0800

    Add server

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MakeNTU2020
+
+## Quick Start
+
+Install packages
+
+```
+pip install -r requirements.txt
+```
+
+Write the following content into `.env`
+
+```
+LINE_CHANNEL_SECRET={your channel secret here}
+LINE_CHANNEL_ACCESS_TOKEN={your channel access token here}
+```
+
+Run the following command
+
+```
+python server.py
+```
---
+++ server.py
@@ -0,0 +1,75 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# ========================================
+
+if os. getenv('MODE') == 'dev':
+    load_dotenv()
+
+PORT = os.getenv('PORT', 8000)
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET', None)
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+# ========================================
+
+
+app = Flask(__name__)
+
+if LINE_CHANNEL_SECRET is None or LINE_CHANNEL_ACCESS_TOKEN is None:
+    print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN' +
+          'as environment variables.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+# ========================================
+
+
+@app.route("/")
+def index():
+    return "Hi"
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    """
+    Webhook callback endpoint
+    """
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    print(signature)
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature." +
+              "Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # Decide what Component to return to Channel
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == '__main__':
+    app.run(port=PORT)
