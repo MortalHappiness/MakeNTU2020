aba6d3d007e2d263a20c1bfa350df4a54ce6ae0e
wulunghui <b07901019@ntu.edu.tw>
2020-10-10 14:37:45 +0800

    addrichmenu

--- richmenu.py
+++ richmenu.py
@@ -1,7 +1,12 @@
 import requests
 import json
+import os
 
 from dotenv import load_dotenv
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 
 # ========================================
 
@@ -15,43 +20,27 @@ if LINE_CHANNEL_ACCESS_TOKEN is None:
 
 # ========================================
 
-headers = {f"Authorization": "Bearer {LINE_CHANNEL_ACCESS_TOKEN}",
+headers = {"Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}",
            "Content-Type": "application/json"}
 
-body = {
-    "size": {"width": 2500, "height": 1686},
-    "selected": "true",
-    "name": "Controller",
-    "chatBarText": "Controller",
-    "areas": [
-        {
-            "bounds": {"x": 551, "y": 325, "width": 321, "height": 321},
-            "action": {"type": "message", "text": "up"}
-        },
-        {
-            "bounds": {"x": 876, "y": 651, "width": 321, "height": 321},
-            "action": {"type": "message", "text": "right"}
-        },
-        {
-            "bounds": {"x": 551, "y": 972, "width": 321, "height": 321},
-            "action": {"type": "message", "text": "down"}
-        },
-        {
-            "bounds": {"x": 225, "y": 651, "width": 321, "height": 321},
-            "action": {"type": "message", "text": "left"}
-        },
-        {
-            "bounds": {"x": 1433, "y": 657, "width": 367, "height": 367},
-            "action": {"type": "message", "text": "btn b"}
-        },
-        {
-            "bounds": {"x": 1907, "y": 657, "width": 367, "height": 367},
-            "action": {"type": "message", "text": "btn a"}
-        }
-    ]
-}
+with open("./richmenubody.json") as fin:
+    body = json.load(fin)
 
 res = requests.request('POST', 'https://api.line.me/v2/bot/richmenu',
                        headers=headers, data=json.dumps(body).encode('utf-8'))
 
-print(res.text)
+if not res.ok:
+    print(res.text)
+    exit()
+
+richmenu_id = json.loads(res.text)["richMenuId"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+with open("images/bottom_menu.png", 'rb') as f:
+    line_bot_api.set_rich_menu_image(richmenu_id, "image/jpeg", f)
+
+req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/'+richmenu_id, 
+                       headers=headers)
+
+print(req.text)
